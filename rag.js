bd30b37f0a3878bbef4deb1ca0d41f6cc8ea0ef6
rahulsahu-18 <rahulsahu770121@gmail.com>
2026-08-22 21:54:27 +0530

    create vector embading of chunk and understand about hugging-face

--- rag.js
+++ rag.js
@@ -1,17 +1,46 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/huggingface_transformers";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 async function main() {
+  // 1. Load PDF
   const loader = new PDFLoader(
     "./285_OOPS lecture notes Complete.pdf",
-    {splitPages:false}
+    {
+      splitPages: false,
+    }
   );
 
-  const doc = await loader.load();
- const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 400, chunkOverlap: 100 })
-  const texts = await splitter.splitText(doc[0].pageContent);
-  console.log(texts.length)
+  const docs = await loader.load();
+
+  // 2. Split text into chunks
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 150,
+  });
+
+  const chunks = await splitter.splitText(docs[0].pageContent);
+
+  console.log("Total chunks:", chunks.length);
+
+  // 3. Create embedding model
+  const embeddings = new HuggingFaceTransformersEmbeddings({
+    model: "Xenova/all-MiniLM-L6-v2",
+  });
+
+  // 4. Generate embedding for first chunk
+  const vector = await embeddings.embedQuery(chunks[0]);
+
+  console.log("First chunk:");
+  console.log(chunks[0]);
+
+  console.log("\nEmbedding:");
+  console.log(vector);
+
+  console.log("\nEmbedding dimensions:", vector.length);
 }
 
 main();
